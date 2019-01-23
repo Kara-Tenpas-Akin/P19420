a79e159dfaa0d36f87071603c0d0f13ee572f5f1
Kara Tenpas <32903579+Kara-Tenpas-Akin@users.noreply.github.com>
2019-01-23 15:04:27 -0500

    Upload
    Test code for Raspberry Pi GUI using Python

---
+++ GUI.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)                 
+        self.master = master
+        self.init_window()
+
+    #Creation of init_window
+    def init_window(self):    
+        self.master.title("GUI")
+        self.pack(fill=BOTH, expand=1)
+    
+        #lbl = Label(self, text="Blackberry High Tunnel")
+        #lbl.grid(column=0, row=0)
+
+        tab_control = ttk.Notebook(self)
+ 
+        tab1 = ttk.Frame(tab_control)
+        tab2 = ttk.Frame(tab_control)
+        tab3 = ttk.Frame(tab_control)
+ 
+        tab_control.add(tab1, text='Status')
+        quitButton = Button(tab1, text="Emergency Stop", command=self.client_exit, bg="white", fg="red")
+        quitButton.place(x=430,y=5)
+        downloadButton = Button(tab1, command=self.download, text="Download Data")
+        downloadButton.place(x=435,y=410)
+        
+        tab_control.add(tab2, text='Controls')
+        quitButton = Button(tab2, text="Emergency Stop", command=self.client_exit, bg="white", fg="red")
+        quitButton.place(x=430,y=5)
+        fertButton = Button(tab2, text="Fertigate", command=self.fertigate, fg="green")
+        fertButton.place(x=150,y=300)
+        waterButton = Button(tab2, text="Water", command=self.water, bg="white", fg="blue")
+        waterButton.place(x=250,y=300)
+        heatButton = Button(tab2, text="Heat", command=self.heat, bg="white", fg="orange")
+        heatButton.place(x=350,y=300)
+        
+        tab_control.add(tab3, text='Error Status')
+        quitButton = Button(tab3, text="Emergency Stop", command=self.client_exit, bg="white", fg="red")
+        quitButton.place(x=430,y=5)
+
+        tab_control.pack(expand=1, fill='both')
+        
+    def client_exit(self):
+        exit()
+        
+    def download(self):
+        messagebox.showinfo('Download Data', 'Download completed.')
+
+    def fertigate(self):
+        messagebox.askyesnocancel('Fertigation System', 'Turn on fertigation system?')
+
+    def water(self):
+        messagebox.askyesnocancel('Irrigation System', 'Turn on irragation system?')
+
+    def heat(self):
+        messagebox.askyesnocancel('Heating System', 'Turn on heating system?')
+        
+root = Tk()
+
+root.geometry("600x500")
+
+app = Window(root)
+root.mainloop()  
+
+
+
