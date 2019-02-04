cebdf7c80cc676c60ab9ae6e694ce5588d6c598e
Kara Tenpas-Akin <krt8036@rit.edu>
2019-02-04 15:15:22 -0500

    cabeee- changed GUI.py, tested test.py, and it works

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-import matplotlib.pyplot as plt
-
+import Pmw
+Pmw.initialise(root)
 
 class Window(Frame):
 
@@ -24,37 +24,37 @@ class Window(Frame):
         tab1 = ttk.Frame(tab_control)
         tab2 = ttk.Frame(tab_control)
         tab3 = ttk.Frame(tab_control)
- 
+
         tab_control.add(tab1, text='Status')
-        quitButton = Button(tab1, text="Emergency Stop", command=self.client_exit, bg="white", fg="red")
+        quitButton = Button(tab1, text="Emergency Stop", command=self.client_exit, bg="red", fg="white")
         quitButton.place(x=430,y=5)
         downloadButton = Button(tab1, command=self.download, text="Download Data")
         downloadButton.place(x=435,y=410)
         
         tab_control.add(tab2, text='Controls')
-        quitButton = Button(tab2, text="Emergency Stop", command=self.client_exit, bg="white", fg="red")
+        quitButton = Button(tab2, text="Emergency Stop", command=self.client_exit, bg="red", fg="white")
         quitButton.place(x=430,y=5)
-        fertButton = Button(tab2, text="Fertigate", command=self.fertigate, fg="green")
+        fertButton = Button(tab2, text="Fertigate", command=self.fertigate, bg="green", fg="white")
         fertButton.place(x=150,y=300)
-        waterButton = Button(tab2, text="Water", command=self.water, bg="white", fg="blue")
-        waterButton.place(x=250,y=300)
-        heatButton = Button(tab2, text="Heat", command=self.heat, bg="white", fg="orange")
+        waterButton = Button(tab2, text="Water", command=self.water, bg="blue", fg="white")
+        waterButton.place(x=260,y=300)
+        heatButton = Button(tab2, text="Heat", command=self.heat, bg="orange", fg="white")
         heatButton.place(x=350,y=300)
         
         tab_control.add(tab3, text='Error Status')
-        quitButton = Button(tab3, text="Emergency Stop", command=self.client_exit, bg="white", fg="red")
+        quitButton = Button(tab3, text="Emergency Stop", command=self.client_exit, bg="red", fg="white")
         quitButton.place(x=430,y=5)
 
         tab_control.pack(expand=1, fill='both')
         
     def client_exit(self):
         exit()
-        
+
     def download(self):
         messagebox.showinfo('Download Data', 'Download completed.')
 
     def fertigate(self):
-        messagebox.askyesnocancel('Fertigation System', 'Turn on fertigation system?')
+        messagebox.askyesnocancel('Fetigation System', 'Turn on fertigation system?')
 
     def water(self):
         messagebox.askyesnocancel('Irrigation System', 'Turn on irragation system?')
@@ -68,6 +68,3 @@ root.geometry("600x500")
 
 app = Window(root)
 root.mainloop()  
-
-
-
