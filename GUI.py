831edd3ce8213b17f9e4f2f3d3c687b1ab51b99e
Kara Tenpas-Akin <krt8036@rit.edu>
2019-02-11 15:54:35 -0500

    krt8036-Updated GUI.py to import text files to plot on GUI. Added sample data files.

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 import Pmw                      # The Python MegaWidget package
 import math
+import csv
 
 ncurves = 4                  # draw 4 curves
 npoints = 7                  # use  7 points on each curve
@@ -57,42 +58,55 @@ class Window(Frame):
         ncurves = 4                  # draw 4 curves
         npoints = 7                  # use  7 points on each curve
 
-        vector_x = []                 # make vector for x-axis
-        vector_y = []
-        # fill data in vectors:
-        for y in range(ncurves):
-            vector_y.append([])      
+        list_x = []                 # make vector for x-axis
+        list_y = []
+        list_x2 = []                 # make vector for x-axis
+        list_y2 = []
 
-        for x in range(npoints):    
-            vector_x.append(x*0.1)   
-            # fill vectors with cool graphs
-            for c in range(ncurves): 
-                vector_y[c].append(math.sin(c*x*0.5))
+        with open('data.txt','r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            for row in plots:
+                list_x.append(int(row[0]))
+                list_y.append(int(row[1]))
+
+        with open('data2.txt','r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            for row in plots:
+                list_x2.append(int(row[0]))
+                list_y2.append(int(row[1]))
+        
 
         g = Pmw.Blt.Graph(tab1)                     # make a new graph area
         g.pack(expand=1, fill='both',padx=10, pady=100)
 
         color = ['red', '#ff9900', 'blue', '#00cc00', 'black', 'grey']
 
-        for c in range(ncurves):                
-            curvename = 'sin(' + str(c) +'x)'    
-            g.line_create(curvename,             
-                          xdata=tuple(vector_x),  
-                          ydata=tuple(vector_y[c]),
-                          color=color[c],          
-                          smooth='natural',        
-                          linewidth=2,             
-                          symbol='')
+        #for c in range(ncurves):                
+        curvename = str('Temp 1')
+        g.line_create(curvename,             
+                        xdata=tuple(list_x),  
+                        ydata=tuple(list_y),
+                        color=color[0],          
+                        smooth='natural',        
+                        linewidth=2,             
+                        symbol='')
+        #for c in range(ncurves):                
+        curvename = str('Temp 2')
+        g.line_create(curvename,             
+                        xdata=tuple(list_x2),  
+                        ydata=tuple(list_y2),
+                        color=color[1],          
+                        smooth='natural',        
+                        linewidth=2,             
+                        symbol='')
     
         g.configure(title='Temperature')          # enter a title
-
 
         # make s row of buttons
         buttons = Pmw.ButtonBox(tab1, labelpos='n', label_text='Options')
         buttons.pack(fill='both', expand=1, padx=200, pady=10)
 
         buttons.add('Grid', command=g.grid_toggle)
-        buttons.add('Smooth',command=self.smooth)
 
 
 # Functions
@@ -111,17 +125,6 @@ class Window(Frame):
 
     def heat(self):
         messagebox.askyesnocancel('Heating System', 'Turn on heating system?')
-            
-    def smooth(self):
-        smoothing = 'linear'
-    
-        if smoothing == 'linear': smoothing='quadratic'
-        elif smoothing == 'quadratic': smoothing='natural'
-        elif smoothing == 'natural': smoothing='step'
-        else: smoothing = 'linear'
-
-        for curvename in g.element_show():
-            g.element_configure(curvename, smooth=smoothing)
 
 
 
