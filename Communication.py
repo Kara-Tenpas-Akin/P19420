8288ca742469b9b7b17c37063af6d95332a77852
Kara Tenpas-Akin <krt8036@rit.edu>
2019-02-20 14:00:41 -0500

    krt8036-Added threading

--- Communication.py
+++ Communication.py
@@ -1,1 +1,18 @@
 # Communication from Pi to Arduino
+
+import threading
+import time
+
+class commThread2(threading.Thread):
+    def __init__(self,name):
+        threading.Thread.__init__(self)
+        self.name = name
+        print("Comm thread is initialized")
+
+    def sun(self):
+        #does stuff here
+        #call run() to execute blocking
+        #csll start() to execute non-blocking
+        while(True):
+            print("Comm Thread is running")
+            time.sleep(2)
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # Main File
 # Run GUI.py and Communition.py
-import threading
 
-def func1():
-    print 'Working'
+#!/usr/bin/env python3
 
-def func2():
-    print 'Working'
+from threading import Timer
+from guiThread1 import Thread1
+from commThread2 import Thread2
 
-if __name__ == '__main__':
-    Thread(target = func1).start()
-    Thread(target = func2).start()
+guiThread = Thread1('guiThread1')  
+guiThread.start()   #starts new thread and continues (non-blocking)
+guiThread.run()     #calls run in this same thread (blocking)
+
+commThread = Thread2('commThread2')
+commThread.start() #starts new thread and continues (non-blocking)
+commThread.run()   #calls run in this same thread (blocking)
+
+"""if__name__== "__main__":
+run()"""
